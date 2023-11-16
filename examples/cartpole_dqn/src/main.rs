f92400a00aaa86c4ca239f9ecd1ec78c574048f7
Jason Thomas <katsoftwarellc@gmail.com>
2023-11-16 14:40:29 -0500

    Event driven / multi-threaded architecture (#3)
    Event driven / multi-threaded architecture

--- examples/cartpole_dqn/src/main.rs
+++ examples/cartpole_dqn/src/main.rs
@@ -1,171 +1,67 @@
-use burn::tensor::backend::{ADBackend, Backend};
-use burn::backend::WgpuBackend;
-use burn::{LearningRate, nn};
-use burn::module::Module;
-use burn::nn::loss::{MSELoss, Reduction};
-use burn::optim::{AdamConfig, GradientsParams, Optimizer};
-use burn::tensor::{Data, Int, Shape, Tensor};
-use burn_autodiff::ADBackendDecorator;
-use burn_wgpu::AutoGraphicsApi;
-use rand::distributions::{Distribution, Uniform};
-use rand::Rng;
-use deep_thinker::model::Environment;
-use deep_thinker::replay_buffer::ReplayBuffer;
+use std::thread;
+use crossbeam_channel::Sender;
+use deep_thinker::model::{DQNConfig, Environment, local_dqn_agent, Agent, AgentMessage, ChannelAgentProxy};
 
 mod cartpole;
 
-type MyBackend = WgpuBackend<AutoGraphicsApi, f32, i32>;
-type MyAutodiffBackend = ADBackendDecorator<MyBackend>;
-
-#[derive(Module, Debug)]
-struct QNetwork<B: Backend> {
-    fc1: nn::Linear<B>,
-    fc2: nn::Linear<B>,
-    fc3: nn::Linear<B>,
-    activation: nn::ReLU,
-}
-
-impl<B: ADBackend> QNetwork<B> {
-    pub fn new(input_size: usize, fc1_size: usize, fc2_size: usize, output_size: usize) -> Self {
-        let fc1 = nn::LinearConfig::new(input_size, fc1_size)
-            .with_bias(true)
-            .init();
-        let fc2 = nn::LinearConfig::new(fc1_size, fc2_size)
-            .with_bias(true)
-            .init();
-        let fc3 = nn::LinearConfig::new(fc2_size, output_size)
-            .with_bias(true)
-            .init();
-
-        Self {
-            fc1,
-            fc2,
-            fc3,
-            activation: nn::ReLU::new(),
-        }
-    }
-
-    pub fn forward(&self, input: Tensor<B, 2>) -> Tensor<B, 2> {
-        let x = self.fc1.forward(input);
-        let x = self.activation.forward(x);
-        let x = self.fc2.forward(x);
-        let x = self.activation.forward(x);
-        let actions = self.fc3.forward(x);
-
-        actions
-    }
-}
-
-fn linear_schedule(start_e: f32, end_e: f32, duration: f32, t: i32) -> f32 {
-    let slope = (end_e - start_e) / duration as f32;
-    end_e.max(slope * t as f32 + start_e)
-}
-
 fn main() {
-    let mut rng = rand::thread_rng();
-
-    // TODO: allow arguments to be passed in
-    let total_timesteps = 500_000;
-    let start_e: f32 = 1.0;
-    let end_e: f32 = 0.05;
-    let exploration_fraction: f32 = 0.5;
-    let learning_starts = 10_000;
-    let train_frequency = 10;
-    //let batch_size = 5;
-    let batch_size = 128;
-    let learning_rate: LearningRate = 0.00025;
-    let target_network_frequency = 500;
-    let gamma = Tensor::<MyAutodiffBackend, 2>::full([batch_size, 1], 0.99);
-    let ones = Tensor::<MyAutodiffBackend, 2>::full([batch_size, 1], 1.0);
-
-    // TODO: get input/output size from environment
+    let num_inputs = 4;
     let num_actions = 2;
-    let random_action_sampler = Uniform::from(0..num_actions as i32);
 
-    let mut env = cartpole::Cartpole::new();
+    let config = DQNConfig {
+        start_epsilon: 1.0,
+        end_epsilon: 0.05,
+        exploration_fraction: 0.5,
+        learning_starts: 10_000,
+        train_frequency: 10,
+        batch_size: 128,
+        learning_rate: 0.00025,
+        target_network_frequency: 500,
+        gamma: 0.99,
+        num_inputs,
+        num_actions,
+        hidden_1_size: 120,
+        hidden_2_size: 84,
+        total_timesteps: 500_000,
+    };
 
-    let mut q_network = QNetwork::<MyAutodiffBackend>::new(4, 120, 84, num_actions);
-    //let mut q_network = QNetwork::<MyAutodiffBackend>::new(4, 10, 10, num_actions);
-    let optimizer = AdamConfig::new();
-    let mut optim = optimizer.init();
-    let mut target_network = q_network.clone();
+    let agent_sender = local_dqn_agent(config);
 
-    let mut rb = ReplayBuffer::new(10_000);
+    let env_count = 10;
+    let mut handles = Vec::with_capacity( env_count);
+    for i in 0..env_count {
+        println!("Spawning env {}", i);
+        let handle = spawn_environment(format!("env{}", i), agent_sender.clone(), 500);
+        handles.push(handle);
+    }
 
-    let mut obs = env.reset();
-    let mut episode_reward = 0.0;
-    let mut episode_number = 0;
-
-    for global_step in 0..total_timesteps {
-        let epsilon = linear_schedule(start_e, end_e, exploration_fraction * total_timesteps as f32, global_step);
-
-        let action = if rng.gen::<f32>() < epsilon {
-            random_action_sampler.sample(&mut rng)
-        } else {
-            let data = Data::new(obs.clone(), Shape::new([1, obs.len()]));
-            let input = Tensor::<MyAutodiffBackend, 2>::from_data(data);
-            let action = q_network.forward(input).argmax(1).into_scalar();
-            action
-        };
-
-        let (next_obs, reward, done) = env.step(action);
-        rb.add(obs.clone(), action, reward, next_obs.clone(), done);
-        episode_reward += reward;
-
-        obs = if done {
-            println!("Episode {}, reward {}, global_step {}", episode_number, episode_reward, global_step);
-            episode_reward = 0.0;
-            episode_number += 1;
-            env.reset()
-        } else {
-            next_obs
-        };
-
-        if global_step > learning_starts {
-            if global_step % train_frequency == 0 {
-                let (obs, actions, rewards, next_obs, dones) = rb.sample(batch_size);
-
-                let next_obs_tensor = vecs_to_tensor(next_obs);
-                let target = target_network.forward(next_obs_tensor);
-                let target_max = target.clone().max_dim(1);
-
-                let rewards_data = Data::new(rewards.clone(), Shape::new([rewards.len(), 1]));
-                let rewards_tensor = Tensor::<MyAutodiffBackend, 2>::from_data(rewards_data);
-
-                let dones_data = Data::new(dones.clone(), Shape::new([dones.len(), 1]));
-                let dones_tensor = Tensor::<MyAutodiffBackend, 2>::from_data(dones_data);
-
-                let td_target = rewards_tensor + gamma.clone() * target_max * (ones.clone() - dones_tensor);
-
-                let obs_tensor = vecs_to_tensor(obs);
-                let q_values = q_network.forward(obs_tensor);
-
-                let actions_data = Data::new(actions.clone(), Shape::new([actions.len(), 1]));
-                let actions_tensor = Tensor::<MyAutodiffBackend, 2, Int>::from_data(actions_data);
-                let old_val = q_values.gather(1, actions_tensor);
-
-                let loss = MSELoss::new().forward(old_val.clone(), td_target, Reduction::Mean);
-
-                let gradients = loss.backward();
-                let gradient_params = GradientsParams::from_grads(gradients, &q_network);
-
-                q_network = optim.step(learning_rate, q_network, gradient_params);
-            }
-
-            if global_step % target_network_frequency == 0 {
-                target_network = q_network.clone();
-            }
-        }
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
 
-// TODO: probably a better way to do this
-fn vecs_to_tensor(input: Vec<Vec<f32>>) -> Tensor<MyAutodiffBackend, 2> {
-    let input_vectors = input.iter().map(|v|
-        {
-            let data = Data::new(v.clone(), Shape::new([1, v.len()]));
-            Tensor::<MyAutodiffBackend, 2>::from_data(data)
-        }).collect::<Vec<Tensor<MyAutodiffBackend, 2>>>();
-    let t = Tensor::cat(input_vectors, 0);
-    t
+fn spawn_environment(env_id: String, agent_sender: Sender<AgentMessage>, episode_count: i32) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        let mut env = cartpole::Cartpole::new();
+
+        let agent = ChannelAgentProxy::new(env_id.clone(), agent_sender.clone());
+        let mut episode_reward = 0.0;
+        for episode in 0..episode_count {
+            let mut obs = env.reset();
+            let mut action = agent.get_first_action(obs.clone());
+            loop {
+                let (next_obs, reward, done) = env.step(action);
+                obs = next_obs;
+
+                episode_reward += reward;
+                action = agent.get_action(obs.clone(), reward, done);
+
+                if done {
+                    println!("Env id {}, Episode {}, Reward {}", env_id, episode, episode_reward);
+                    episode_reward = 0.0;
+                    break;
+                }
+            }
+        }
+    })
 }
