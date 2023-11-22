5c9702f192bdc06a233deac33a4f2ae5baa240ae
jason <katsoftwarellc@gmail.com>
2023-11-21 21:58:10 -0500

    zenoh

--- examples/cartpole_dqn_zenoh/src/main.rs
+++ examples/cartpole_dqn_zenoh/src/main.rs
@@ -1,3 +1,89 @@
-fn main() {
-    println!("Hello, world!");
+//
+// Copyright (c) 2023 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+use clap::{App, Arg};
+use zenoh::config::Config;
+use zenoh::prelude::r#async::*;
+
+#[async_std::main]
+async fn main() {
+    // initiate logging
+    env_logger::init();
+
+    let (config, key_expr, value) = parse_args();
+
+    println!("Opening session...");
+    let session = zenoh::open(config).res().await.unwrap();
+
+    println!("Putting Float ('{key_expr}': '{value}')...");
+    session.put(&key_expr, value).res().await.unwrap();
+
+    session.close().res().await.unwrap();
+}
+
+//
+// Argument parsing -- look at the main for the zenoh-related code
+//
+fn parse_args() -> (Config, String, f64) {
+    let default_value = std::f64::consts::PI.to_string();
+
+    let args = App::new("zenoh put float example")
+        .arg(
+            Arg::from_usage("-m, --mode=[MODE] 'The zenoh session mode (peer by default).")
+                .possible_values(["peer", "client"]),
+        )
+        .arg(Arg::from_usage(
+            "-e, --connect=[ENDPOINT]...  'Endpoints to connect to.'",
+        ))
+        .arg(Arg::from_usage(
+            "-l, --listen=[ENDPOINT]...   'Endpoints to listen on.'",
+        ))
+        .arg(Arg::from_usage(
+            "-c, --config=[FILE]      'A configuration file.'",
+        ))
+        .arg(
+            Arg::from_usage("-k, --key=[KEYEXPR]        'The key expression to put.'")
+                .default_value("demo/example/zenoh-rs-put"),
+        )
+        .arg(
+            Arg::from_usage("-v, --value=[VALUE]      'The float value to put.'")
+                .default_value(&default_value),
+        )
+        .arg(Arg::from_usage(
+            "--no-multicast-scouting 'Disable the multicast-based scouting mechanism.'",
+        ))
+        .get_matches();
+
+    let mut config = if let Some(conf_file) = args.value_of("config") {
+        Config::from_file(conf_file).unwrap()
+    } else {
+        Config::default()
+    };
+    if let Some(Ok(mode)) = args.value_of("mode").map(|mode| mode.parse()) {
+        config.set_mode(Some(mode)).unwrap();
+    }
+    if let Some(values) = args.values_of("connect") {
+        config.connect.endpoints = values.map(|v| v.parse().unwrap()).collect();
+    }
+    if let Some(values) = args.values_of("listen") {
+        config.listen.endpoints = values.map(|v| v.parse().unwrap()).collect();
+    }
+    if args.is_present("no-multicast-scouting") {
+        config.scouting.multicast.set_enabled(Some(false)).unwrap();
+    }
+
+    let key_expr = args.value_of("key").unwrap().to_string();
+    let value: f64 = args.value_of("value").unwrap().parse().unwrap();
+
+    (config, key_expr, value)
 }
